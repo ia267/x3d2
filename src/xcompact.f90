ffbeb3a60168dd6e5771f9551cd61d7e2a6f1410
Sebastien Lemaire <s.lemaire@epcc.ed.ac.uk>
2025-07-16 20:17:08 +0100

    add foil test case

--- src/xcompact.f90
+++ src/xcompact.f90
@@ -7,6 +7,7 @@ program xcompact
   use m_common, only: pi, get_argument, VERT
   use m_config, only: domain_config_t, solver_config_t
   use m_mesh
+  use m_case_foil, only: case_foil_t
   use m_case_channel, only: case_channel_t
   use m_case_generic, only: case_generic_t
   use m_case_tgv, only: case_tgv_t
@@ -119,6 +120,9 @@ program xcompact
   case ('tgv')
     allocate (case_tgv_t :: flow_case)
     flow_case = case_tgv_t(backend, mesh, host_allocator)
+  case ('foil')
+    allocate (case_foil_t :: flow_case)
+    flow_case = case_foil_t(backend, mesh, host_allocator)
   case default
     error stop 'Undefined flow_case.'
   end select
