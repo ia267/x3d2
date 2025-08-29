afd299dbecc2628c75ebb1137d5b971301b470a7
Irufan Ahmed <ia267@users.noreply.github.com>
2025-08-29 21:12:42 +0000

    add sponge layer by modifying RHS for momentum eq

--- src/case/foil.f90
+++ src/case/foil.f90
@@ -13,12 +13,14 @@ module m_case_foil
   implicit none
 
   type, extends(base_case_t) :: case_foil_t
+    class(field_t), pointer :: ramp_dev => null()
   contains
     procedure :: boundary_conditions => boundary_conditions_foil
     procedure :: initial_conditions => initial_conditions_foil
     procedure :: forcings => forcings_foil
     procedure :: pre_correction => pre_correction_foil
     procedure :: postprocess => postprocess_foil
+    procedure :: case_finalise => finalise_foil
   end type case_foil_t
 
   interface case_foil_t
@@ -44,54 +46,34 @@ contains
 
     class(case_foil_t) :: self
 
-    integer, dimension(3) :: dims
-    integer :: damping_layer_size
-    class(field_t), pointer :: ramp
-    class(field_t), pointer :: ramp_dev
-
-    integer :: j, l
-
-    ! Number of cells used in the damping region
-    damping_layer_size = 50
-
-    dims = self%solver%mesh%get_dims(VERT)
-    ramp => self%solver%host_allocator%get_block(DIR_C)
-
-    do j = 1, dims(2) - damping_layer_size
-      ramp%data(:, j, :) = 1.0_dp
-    end do
-
-    do l = 1, damping_layer_size
-      j = dims(2) - damping_layer_size + l
-      ramp%data(:, j, :) = cos(pi*l/(damping_layer_size*2.0_dp))
-    end do
-
-    ramp_dev => self%solver%backend%allocator%get_block(DIR_X)
-    call self%solver%backend%set_field_data(ramp_dev, ramp%data)
-
-    call self%solver%host_allocator%release_block(ramp)
-
-
-    ! Gradially dampen velocity
-    call self%solver%backend%vecmult(self%solver%u, ramp_dev)
-    call self%solver%backend%vecmult(self%solver%v, ramp_dev)
-    call self%solver%backend%vecmult(self%solver%w, ramp_dev)
-
-    ! Gradually force V to be inlet velocity
-    ! construct: U_inlet * (1 - ramp)
-    call self%solver%backend%field_scale(ramp_dev, -1.0_dp)
-    call self%solver%backend%field_shift(ramp_dev, 1.0_dp)
-    call self%solver%backend%field_scale(ramp_dev, 14.5_dp)
-    call self%solver%backend%vecadd(1.0_dp, ramp_dev, 1.0_dp, self%solver%v)
-
-    call self%solver%backend%allocator%release_block(ramp_dev)
-
   end subroutine boundary_conditions_foil
 
   subroutine initial_conditions_foil(self)
     implicit none
 
     class(case_foil_t) :: self
+
+    integer, dimension(3) :: dims
+    integer :: damping_layer_size
+    class(field_t), pointer :: ramp_host
+    integer :: j, l
+
+    damping_layer_size = 50
+    dims = self%solver%mesh%get_dims(VERT)
+
+    ramp_host => self%solver%host_allocator%get_block(DIR_C)
+    do j = 1, dims(2) - damping_layer_size
+      ramp_host%data(:, j, :) = 1.0_dp
+    end do
+    do l = 1, damping_layer_size
+      j = dims(2) - damping_layer_size + l
+      ramp_host%data(:, j, :) = cos(pi*l/(damping_layer_size*2.0_dp))
+    end do
+
+    self%ramp_dev => self%solver%backend%allocator%get_block(DIR_X)
+    call self%solver%backend%set_field_data(self%ramp_dev, ramp_host%data)
+
+    call self%solver%host_allocator%release_block(ramp_host)
 
     call self%solver%u%fill(0.0_dp)
     call self%solver%v%fill(14.5_dp)
@@ -110,7 +92,8 @@ contains
     class(field_t), intent(inout) :: du, dv, dw
     integer, intent(in) :: iter
 
-    ! do nothing for foil case
+    call self%solver%backend%apply_sponge_layer(self%solver%v, dv, self%ramp_dev, 14.5_dp)
+
   end subroutine forcings_foil
 
   subroutine pre_correction_foil(self, u, v, w)
@@ -136,5 +119,18 @@ contains
     call self%print_div_max_mean(self%solver%u, self%solver%v, self%solver%w)
 
   end subroutine postprocess_foil
+
+  subroutine finalise_foil(self)
+    implicit none
+    class(case_foil_t) :: self
+  
+    if (associated(self%ramp_dev)) then
+      call self%solver%backend%allocator%release_block(self%ramp_dev)
+    end if
+
+    ! Call parent's finalisation
+    call self%base_case_t%case_finalise()
+
+  end subroutine finalise_foil
 
 end module m_case_foil
