1379118708af616ce685e8a5625db114fcd7dab4
Irufan Ahmed <irufan.ahmed04@imperial.ac.uk>
2025-09-24 18:44:23 +0100

    use fortran ordering for ibm mask

--- src/module/ibm.f90
+++ src/module/ibm.f90
@@ -73,27 +73,17 @@ contains
 
       ! Read the vertex mask ep1
       !
-      ! The mask was written in python in C order
-      ! start_dims and count_dims are thus reversed
-      ! The resulting output is in reversed order
       dims = mesh%get_dims(VERT)
-      start_dims = int(ibm%mesh%par%n_offset(3:1:-1), i8)
-      count_dims = int(dims(3:1:-1), i8)
+      start_dims = int(ibm%mesh%par%n_offset, i8)
+      count_dims = int(dims, i8)
 
-      ! Allocate field_data with the expected (reversed) dimensions from the read
-      allocate (field_data(count_dims(1), count_dims(2), count_dims(3)))
-      call reader_session%read_data("ep1", field_data, start_dims, count_dims)
+      ! Allocate field_data with the expected dimensions from the read
+      allocate(field_data(count_dims(1), count_dims(2), count_dims(3)))
+      call io_session%read_data("ep1", field_data, start_dims, count_dims)
 
       ! Get and fill a block on the host
-      ! The order of the data is corrected in the loop below
       ep1 => ibm%host_allocator%get_block(DIR_C)
-      do i = 1, dims(1)
-        do j = 1, dims(2)
-          do k = 1, dims(3)
-            ep1%data(i, j, k) = field_data(k, j, i)
-          end do
-        end do
-      end do
+      ep1%data(:, :, :) = field_data(:, :, :)
 
       ! Get a block on the device and move the data
       ibm%ep1 => ibm%backend%allocator%get_block(DIR_X)
